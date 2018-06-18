59d186a63a032f1e4569b48e141d50781ed74628
mans2singh <mans2singh@yahoo.com>
2018-06-17 20:31:31 -0700

    Added Rdbms based flow file template

---
+++ dl4jsql/create-test-db.sql
@@ -0,0 +1,7 @@
+create database test;
+
+create table test.test1 (id varchar(20), input varchar(4000), output varchar(400))
+
+alter table test.test1 add PRIMARY KEY(id)
+
+
---
+++ dl4jsql/insert-example-row.sql
@@ -0,0 +1,2 @@
+insert into test.test1 values('1','2.0,.5,.4,0.2','')
+
